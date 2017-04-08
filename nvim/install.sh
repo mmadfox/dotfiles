64b86b33f37cb3c429792c88981ee14ab55d2d42
mmadfox <sergey.liskonog@yandex.ru>
2017-04-08 11:02:17 +0300

    added nvim config

---
+++ nvim/install.sh
@@ -0,0 +1,9 @@
+brew install neovim/neovim/neovim
+pip2 install neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+go get -u github.com/nsf/gocode
+go get github.com/tools/godep
+go get golang.org/x/tools/cmd/goimports
+
+
